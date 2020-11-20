966bec9ac2449eda4dc7b6f06a5bf3b2d87d4f3c
Pavel.Samolovov <Pavel.Samolovov@nexign-systems.com>
2020-11-20 22:34:19 +0700

    Update apiURL

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,7 +1,7 @@
 import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 
-const apiUrl = 'http://localhost:8080';
+const apiUrl = 'http://academfootball.ru:8080';
 const httpClient = fetchUtils.fetchJson;
 
 export default {
